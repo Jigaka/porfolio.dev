dfd865afa33dbfe5a3769bbe5a7faa79db423369
Jose Garcete <garcetejoseka@gmail.com>
2025-02-16 13:58:55 -0300

    Se corrige base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import robotsTxt from "astro-robots-txt"
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://jigaka.github.io',
-  base: '/github-pages',
+  base: '/porfolio.dev',
 })
