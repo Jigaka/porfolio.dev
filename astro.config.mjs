5f1f095127a6a0b116574885b75c0eeea8265c6b
Jose Garcete <garcetejoseka@gmail.com>
2025-02-16 14:02:45 -0300

    Se corrige config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
+//prueba
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://jigaka.github.io',
