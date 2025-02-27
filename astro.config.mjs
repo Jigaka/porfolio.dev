00595d5f39048956b924b628cfbcbcd043ae1802
Jose Garcete <garcetejoseka@gmail.com>
2025-02-26 22:14:57 -0300

    add google analytics two

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 
+import partytown from "@astrojs/partytown"
+
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 //prueba
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
   site: 'https://jose-garcete.dev',
+  integrations: [
+    tailwind(), 
+    robotsTxt(),
+    partytown({
+      config: {
+        forward: ["datalayer.push"],
+      }
+    })],
 })
